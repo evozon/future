923d84a1ba56f1042fd0b9ac765ce41e53bc108e
Calin Pristavu <calinpristavu@gmail.com>
2023-06-12 10:58:23 +0300

    [FT-21] Collect output and display it as a table

--- cmd/future/shutdown.go
+++ cmd/future/shutdown.go
@@ -3,7 +3,9 @@ package future
 import (
 	"fmt"
 	"log"
+	"sort"
 
+	"github.com/jedib0t/go-pretty/v6/table"
 	"github.com/spf13/cobra"
 
 	"future/internal/collector"
@@ -23,15 +25,33 @@ var Shutdown = &cobra.Command{
 			log.Fatalf("could not get the summary: %v\n", err)
 		}
 
-		for _, summaryData := range summaryResponse.GetSummary() {
-			result := "successful"
-			if summaryData.Status != 0 {
-				result = "failed"
-			}
+		summary := summaryResponse.GetSummary()
 
-			fmt.Printf("%s -> [%s] %s", summaryData.Command, result, summaryData.Output)
-		}
+		reorder(summary)
+		render(summary)
 
 		_, _ = client.Shutdown(cmd.Context(), &collector.ShutdownRequest{})
 	},
+}
+
+func render(summary []*collector.SummaryData) {
+	w := table.NewWriter()
+	w.AppendHeader(table.Row{"Command", "Status", "Output"})
+
+	for _, summaryData := range summary {
+		result := "successful"
+		if summaryData.Status != 0 {
+			result = "failed"
+		}
+
+		w.AppendRow(table.Row{summaryData.Command, result, summaryData.Output})
+	}
+
+	fmt.Println(w.Render())
+}
+
+func reorder(summary []*collector.SummaryData) {
+	sort.Slice(summary, func(i, j int) bool {
+		return summary[i].Status > summary[j].Status
+	})
 }
